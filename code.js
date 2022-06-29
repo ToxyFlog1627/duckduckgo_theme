edd96e727f31b4d1ff6a256910477afa402cd59e
ToxyFlog1627 <59206807+ToxyFlog1627@users.noreply.github.com>
2022-06-29 11:00:01 +0300

    Create code.js

---
+++ code.js
@@ -0,0 +1,2 @@
+const theme = ['21=302D41', '7=1E1E2E', '8=D9E0EE', '9=A963D5', 'aa=D5A2F6', 'ae=1E1E2E', 'j=302D41', 'x=A963D5'];
+for (const item of theme) document.cookie = `${item}; max-age=126144000; samesite=lax; secure`;
